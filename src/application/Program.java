ba03b0891e4fcfa4f32e856e95e922a1a1a7cf3f
Felipe Massashi <Felipe Massashi>
2023-03-26 14:14:05 -0300

    melhorando o código

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createdSellerDao();
 		
+		System.out.println("=== TEST 1: seller finById ====");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -45,7 +45,6 @@ public class SellerDaoJDBC implements SellerDao{
 		ResultSet rs = null;
 		
 		try {
-			
 			st = conn.prepareStatement(
 					"SELECT seller.*, department.Name as DepName "
 					+ "FROM seller INNER JOIN department "
@@ -55,36 +54,35 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1,  id);
 			rs = st.executeQuery();
 			if (rs.next()) {
-				
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName("DepName");
-				
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBitrhDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
-				
+				Department dep = instantiateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
 				return obj;
-				
 			}
 			return null;
-			
 		} catch (SQLException e) {
-			
 			throw new DbException(e.getMessage());
-			
 		} finally {
-			
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-			
 		}
-		
-		
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBitrhDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName("DepName");
+		return dep;
 	}
 
 	@Override
