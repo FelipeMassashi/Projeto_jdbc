b7f253e4fdb6f39e6ba04dda98409c7e614b8463
Felipe Massashi <Felipe Massashi>
2023-03-26 16:04:06 -0300

    delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createdSellerDao();
 		
@@ -36,12 +39,19 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("\n=== TEST 4: seller update ====");
-		seller = sellerDao.findById(1);
-		seller.setName("Adilson Martins");
-		sellerDao.update(seller);
-		System.out.println("Update Completed");
+//		System.out.println("\n=== TEST 5: seller update ====");
+//		seller = sellerDao.findById(1);
+//		seller.setName("Adilson Martins");
+//		sellerDao.update(seller);
+//		System.out.println("Update Completed");
+		
+		System.out.println("\n=== TEST 6: seller delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deletedById(id);
+		System.out.println("Delete Completed");
 
+		sc.close();
 	}
 
 }
