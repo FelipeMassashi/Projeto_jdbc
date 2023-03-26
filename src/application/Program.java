1f6db7b8d3c0cdf3a8a99b828fac0489ebb3668c
Felipe Massashi <Felipe Massashi>
2023-03-26 15:00:04 -0300

    findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,8 +15,14 @@ public class Program {
 		
 		System.out.println("=== TEST 1: seller finById ====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("\n=== TEST 2: seller finByDepartment ====");
+		Department dep = new Department(1, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
