8867763001f6310e8b340437b087a78105cd8d21
Felipe Massashi <Felipe Massashi>
2023-03-26 16:15:38 -0300

    departmentDaoJDBC

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createdSellerDao() {
 		return new SellerDaoJDBC(DB.getConnetion());
+	}
+	
+	public static DepartmentDao createdDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnetion());
 	}
 
 }
